1057337a6b3997f0e5e4723e1984ae09ac9048eb
MarkSA81 <markamoras@gmail.com>
2026-04-24 11:12:58 -0300

    Ragefist Constant

--- constants/move_constants.asm
+++ constants/move_constants.asm
@@ -35,7 +35,7 @@
 	const FOCUS_BLAST   ; $1b
 	const TOXIC_SPIKES  ; $1c
 	const HEADBUTT      ; $1d
-	const HORN_ATTACK   ; $1e
+	const RAGE_FIST     ; $1e
 	const TRICK         ; $1f
 	const HEX           ; $20
 	const TACKLE        ; $21
